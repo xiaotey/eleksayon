4e2883f52fcca9ceea92abc70f895b86a0d53704
Siote <fighteus@gmail.com>
2023-06-05 00:32:57 +0800

    Push 2

--- app/src/main/java/com/example/eleksayon/MainActivity.java
+++ app/src/main/java/com/example/eleksayon/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.eleksayon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 //test
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdmin();
+            }
+        });
+
+        public void openAdmin(){
+            Intent intent = new Intent(this, activity_log_in_page.class);
+            startActivity(intent);
+        }
     }
 }
