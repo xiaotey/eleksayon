75440079373ee2ad81f801d6fc911ef15ac103a3
Siote <fighteus@gmail.com>
2023-06-04 22:27:42 +0800

    Push 1

--- app/src/main/java/com/example/eleksayon/LogInPage.java
+++ app/src/main/java/com/example/eleksayon/LogInPage.java
@@ -3,12 +3,12 @@ package com.example.eleksayon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//test
-public class MainActivity extends AppCompatActivity {
+
+public class LogInPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in_page);
     }
 }
