67b0b2fd6d49e15ab7f7f1e390cac416be97899f
Siote <fighteus@gmail.com>
2023-06-07 02:45:29 +0800

    Push 2

--- app/src/main/java/com/example/eleksayon/LogInPage.java
+++ app/src/main/java/com/example/eleksayon/LogInPage.java
@@ -2,13 +2,29 @@ package com.example.eleksayon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInPage extends AppCompatActivity {
+
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+        button4 = findViewById(R.id.button4);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogInPage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/eleksayon/MainActivity.java
+++ app/src/main/java/com/example/eleksayon/MainActivity.java
@@ -9,12 +9,31 @@ import android.widget.Button;
 
 //test
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+
+    Button button2;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        }
+        button2 = findViewById(R.id.button2);
+        button = findViewById(R.id.button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LogInPage.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LogInPage.class);
+                startActivity(intent);
+            }
+        });
     }
+}
