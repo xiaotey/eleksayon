5c2bafe016a02fcdbeae0ec54d21840752d798a5
plenngaul <astilleroglennpaul4212@gmail.com>
2023-06-12 07:26:51 +0800

    admin added

--- app/src/main/java/com/example/eleksayon/DBHandler.java
+++ app/src/main/java/com/example/eleksayon/DBHandler.java
@@ -6,60 +6,54 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class DBHandler extends SQLiteOpenHelper {
 
-    public static final String databaseName = "Register.db";
+    public static final String databaseName_1 = "Students.db";
+    public static final String databaseName_2 = "Admins.db";
 
     public DBHandler(@Nullable Context context) {
-        super(context, "Register.db", null, 1);
+        super(context, "Students.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase MyDatabase) {
-        MyDatabase.execSQL("create Table allusers(email TEXT primary key, password TEXT, id TEXT, course TEXT)");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE allusers(email TEXT PRIMARY KEY, password TEXT, id TEXT, course TEXT)");
+        db.execSQL("CREATE TABLE adminusers(email TEXT PRIMARY KEY, password TEXT,id TEXT, course TEXT)");
 
+        ContentValues adminValues = new ContentValues();
+        adminValues.put("email", "jawhara.amirol@g.msuiit.edu.ph");
+        adminValues.put("password", "12345");
+        adminValues.put("id", "2021-0000");
+        db.insert("adminusers", null, adminValues);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase MyDatabase, int i, int i1) {
-        MyDatabase.execSQL("drop Table if exists allusers");
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS allusers");
+        db.execSQL("DROP TABLE IF EXISTS adminusers");
+        onCreate(db);
     }
 
-    public Boolean insertData(String email, String password, String id, String course){
-        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+    public Boolean insertData(String email, String password, String id, String course) {
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("email", email);
         contentValues.put("password", password);
         contentValues.put("id", id);
         contentValues.put("course", course);
-        long result = MyDatabase.insert("allusers", null, contentValues);
-
-        if(result == -1){
-            return false;
-        }else{
-            return true;
-        }
+        long result = db.insert("allusers", null, contentValues);
+        return result != -1;
     }
-    public Boolean checkEmail(String email){
-        SQLiteDatabase MyDatabase = this.getWritableDatabase();
-        Cursor cursor = MyDatabase.rawQuery("select * from allusers where email = ?", new String[]{email});
 
-        if (cursor.getCount() > 0){
-            return true;
-        }else{
-            return false;
-        }
+    public Boolean checkEmail(String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM allusers WHERE email = ?", new String[]{email});
+        return cursor.getCount() > 0;
     }
-    public Boolean checkEmailPassword(String email, String password){
-        SQLiteDatabase MyDatabase = this.getWritableDatabase();
-        Cursor cursor = MyDatabase.rawQuery("select * from allusers where email = ? and password = ?", new String[]{email, password});
 
-        if (cursor.getCount() > 0){
-            return true;
-        }else{
-            return false;
-        }
+    public Boolean checkEmailPassword(String email, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM allusers WHERE email = ? AND password = ?", new String[]{email, password});
+        return cursor.getCount() > 0;
     }
 }
