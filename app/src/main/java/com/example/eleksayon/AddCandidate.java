84ce83773f7ff2e660510864591dd18ec13afb3d
Nitro 5 <ltraw@gmail.com>
2023-06-22 01:12:21 +0800

    push 7

--- app/src/main/java/com/example/eleksayon/AddCandidate.java
+++ app/src/main/java/com/example/eleksayon/AddCandidate.java
@@ -3,12 +3,19 @@ package com.example.eleksayon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddCandidate extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_candidate);
+
+
     }
 }
--- app/src/main/java/com/example/eleksayon/AdminDashboard.java
+++ app/src/main/java/com/example/eleksayon/AdminDashboard.java
@@ -2,13 +2,49 @@ package com.example.eleksayon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+
 
 public class AdminDashboard extends AppCompatActivity {
+
+    private ImageButton calendarbutton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        calendarbutton = findViewById(R.id.calendarbutton);
+        calendarbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDialog();
+
+            }
+        });
+
+
+
+
+        }
+        private void openDialog(){
+
+            DatePickerDialog dialog = new DatePickerDialog(this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+
+                }
+            }, 2023, 0, 15);
+        }
+
     }
-}
+
