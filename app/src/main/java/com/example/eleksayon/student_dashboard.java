2ec2650fb104dc4344d8e48d8e43fe2b164d39bb
Jesseraxe <moreless949@gmail.com>
2023-07-04 23:20:40 +0800

    Push fixes 4

--- app/src/main/java/com/example/eleksayon/student_dashboard.java
+++ app/src/main/java/com/example/eleksayon/student_dashboard.java
@@ -2,13 +2,26 @@ package com.example.eleksayon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class student_dashboard extends AppCompatActivity {
-
+    public Button vote_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+
+        vote_button = findViewById(R.id.vote_button);
+        vote_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(student_dashboard.this, StudentVoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
