2233ce1fd0e2bc31e0701ee767caeecf8247fd61
Justin <Arabesj3@gmail.com>
2023-07-04 09:05:49 +0800

    partial

---
+++ app/src/main/java/com/example/eleksayon/student_dashboard.java
@@ -0,0 +1,14 @@
+package com.example.eleksayon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+    }
+}
