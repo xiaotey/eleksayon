33d4ec7c4df5c328425aeea267272a3109df95e4
plenngaul <astilleroglennpaul4212@gmail.com>
2023-07-05 17:49:01 +0800

    kini

--- app/src/main/java/com/example/eleksayon/Candidate.java
+++ app/src/main/java/com/example/eleksayon/Candidate.java
@@ -1,28 +1,36 @@
 package com.example.eleksayon;
 
 public class Candidate {
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String yearLevel;
+    private String courseCandidate;
     private String position;
-    private String description;
+    private String platform;
     private String imagePath;
 
-    public Candidate(String name, String position, String description, String imagePath) {
-        this.name = name;
+    public Candidate(String firstName, String lastName, String yearLevel, String courseCandidate, String position, String platform, String imagePath) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearLevel = yearLevel;
+        this.courseCandidate = courseCandidate;
         this.position = position;
-        this.description = description;
+        this.platform = platform;
         this.imagePath = imagePath;
     }
 
-    public String getName() {
-        return name;
+    public String getLastName() {
+        return lastName;
     }
-
+    public String getFirstName() {
+        return firstName;
+    }
     public String getPosition() {
         return position;
     }
 
     public String getDescription() {
-        return description;
+        return platform;
     }
 
     public String getImagePath() {
